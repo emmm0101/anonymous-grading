8a9a0192129db686eefe4283470cab064a512e62
Laura <hulubalaura31@gmail.com>
2021-12-04 22:23:24 +0200

    create tables

--- anonymous-grading/src/db/index.js
+++ anonymous-grading/src/db/index.js
@@ -1,30 +1,83 @@
-const { Sequelize } = require('sequelize');
+const express = require('express')
+const { Sequelize, DataTypes } = require('sequelize');
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const sequelize = new Sequelize({
     dialect: 'sqlite',
     storage: './mindhunter.db'
-  });
+});
 
-// const db  = new sqlite3.Database("./mindhunter.db", sqlite3.OPEN_READWRITE, (err) => {
-//     if(err) return console.error(err.message);
+async function test() {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
 
-//     console.log("connection was succesful");
-// });
+test()
+const project = sequelize.define('project', {
+    projectID: {
+        type: Sequelize.UUID,
+        primaryKey: true
+    },
+    name: Sequelize.TEXT,
+    link: Sequelize.TEXT
+}, {
+    underscored: true,
+    freezeTableName: true
+});
+
+const user = sequelize.define('user', {
+    userID: {
+        type: Sequelize.UUID,
+        primaryKey: true
+    },
+    first_name: Sequelize.TEXT,
+    last_name: Sequelize.TEXT,
+    email: Sequelize.TEXT,
+    password: Sequelize.TEXT,
+    account_type: Sequelize.TEXT,
+    projectID: Sequelize.INTEGER,
+}, {
+    underscored: true,
+    freezeTableName: true
+});
 
 
-const sqlSelect = `SELECT * FROM project`;
+const deliverable = sequelize.define('deliverable', {
+    deliverableID: {
+        type: Sequelize.UUID,
+        primaryKey: true
+    },
+    ddl_date: Sequelize.TEXT,
+}, {
+    underscored: true,
+    freezeTableName: true
+});
 
-// sequelize.all(sqlSelect, [], (err, rows) => {
-//     if(err) return console.error(err.message);
+const member = sequelize.define('member', {
+    grade: Sequelize.REAL,
+}, {
+    underscored: true,
+    freezeTableName: true
+});
 
-//     rows.forEach((row) => {
-//         console.log(row);
-//     })
-// })
+member.hasMany(deliverable, { foreignKey: 'deliverableID'});
+deliverable.belongsTo(member);
+member.hasOne(user, { foreignKey: 'usedID' });
+user.belongsTo(member);
+deliverable.hasOne(project, { foreignKey: 'projectID' });
+project.belongsTo(deliverable);
+user.hasOne(project, { foreignKey: 'projectID' });
+project.belongsTo(user);
 
-// sequelize.close((err) => {
-//     if(err) return console.error(err.message);
-// });
+async function checkIfExists() {
+    await user.sync();
+    await project.sync();
+    await deliverable.sync();
+    await member.sync();
+}
 
-return sequelize.query(sqlSelect, {
-    type:sequelize.QueryTypes.SELECT
-  })
+checkIfExists();
